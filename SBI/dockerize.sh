d53a43635f7f91433ccf7a46117d6c1b6ca1b846
subham mandal <subhammandal01@gmail.com>
2020-05-22 13:16:25 +0530

    updated script

--- SBI/dockerize.sh
+++ SBI/dockerize.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-set -x
 export PATH="$PATH:/usr/local/bin"
 DIR=$(dirname $0)
 source $DIR/lib/function.sh
@@ -7,6 +6,7 @@ source $DIR/scan.sh
 
 TMP_IMG=$(mktemp)
 SCAN_LOG="scan_status.log"
+[ -f $LOGFILE ] && cat /dev/null > $LOGFILE
 [ -s $SCAN_LOG ] && cat /dev/null > $SCAN_LOG
 
 [ $BRANCH_NAME == "master" ] && tag=$BRANCH_NAME-$(version) || tag=$BRANCH_NAME-$(version)-$BUILD_ID
@@ -35,7 +35,6 @@ services=$(cat $(dirname $0)/service-manifest.txt)
 for s in $services
 do
 [ -f $TMP_IMG ] && cat /dev/null > $TMP_IMG
-[ -f $LOGFILE ] && cat /dev/null > $LOGFILE
 docker build -t ${s}:${tag}  -f $(dirname $0)/docker/$dockerfile . | tee -a $TMP_IMG
 start_scan_local "$(cat $TMP_IMG| tail -n1|awk '{print $3}')"
 [ $? -ne 0 ] && { export SCAN_STATUS=fail ; echo "Image has vulnerabilities,Please check and fix" ; } || :
